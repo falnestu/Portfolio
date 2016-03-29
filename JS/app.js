9e3ca4a47720e84e9bee4f921bb660ff021d55dd
falnestu <bastleroy@gmail.com>
2016-03-29 13:40:26 +0200

    Site v1.0 - need fine tunings

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors:[
-        	'home',
-        	'about',
-        	'work',
-        	'contact'
-        	 ]
+        anchors:['home','about','work','contact'],
+        keyboardScrolling: false
     });
+    $('.slider').slick({
+    	dots:true,
+    	arrows:false
+    });
+    $('.btn-toggle-menu').click(function(){
+    	$('.main-menu').toggleClass("hide-menu");
+    })
 });
